2b69f9a8b9211e97de4c6de196dcb0aed89c2ee4
luis perylo <lfpb@cesar.school>
2024-08-23 15:28:58 -0300

    transformando base 10 em hexadecimal

--- main.c
+++ main.c
@@ -3,22 +3,42 @@
 #include <stdlib.h>
 #include <math.h>
 
-void transforma_binario(int n,int resultado[], int *tamanho) {
+    void transforma_binario(int n,int resultado[], int *tamanho) {
 
-    int i = 0;
+        int i = 0;
 
-    while (n > 0) {
-        resultado[i] = n % 2;
-        n = n / 2;
-        i++;
+        while (n > 0) {
+            resultado[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+        *tamanho = i;
     }
-    *tamanho = i;
-}
 
+    void transforma_hexa(int n,char resultadohexa[], int *tamanho) {
+
+        int i = 0;
+
+        do {
+            int resto = n % 16;
+
+            if (resto >= 10) {
+                resultadohexa[i] = 'A' + (resto - 10);
+            } else {
+                resultadohexa[i] = '0' + resto;
+            }
+
+            n = n/16;
+            i++;
+        }while (n > 0);
+
+        *tamanho = i;
+    }
 int main(void) {
 
     int opcao,numero;
     int resultado[32];
+    char resultadohexa[32];
     int tamanho = 0;
 
     do {
@@ -41,18 +61,33 @@ int main(void) {
             }
 
             break;
+
         case 2:
             printf("Digite um numero na Base 10: ");
             scanf("%d",&numero);
+
         case 3:
             printf("Digite um numero na Base 10: ");
             scanf("%d",&numero);
+
+            transforma_hexa(numero, resultadohexa, &tamanho);
+
+            printf("Resultado em hexadecimal: ");
+
+            for (int i = tamanho - 1; i >= 0; i--) {
+                printf("%c", resultadohexa[i]);
+            }
+
+            break;
+
         case 4:
             printf("Digite um numero na Base 10: ");
             scanf("%d",&numero);
+
         case 5:
             printf("Digite um numero na Base 10: ");
             scanf("%d",&numero);
+
         case 6:
             printf("Digite um numero real em decimal: ");
             scanf("%d",&numero);
