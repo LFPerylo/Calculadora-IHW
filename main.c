7855db6b935d68ed0d8a77ec5bf4d483bb059a81
luis perylo <lfpb@cesar.school>
2024-08-23 16:33:16 -0300

    transformar base 10 para BCD/ QUESTÃO 01 PRONTA

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+//Aluno: Luis Felipe Perylo Benicio, 3 periodo, Turma A
+//transformar base 10 para bcd: 16:30 23/08/2024
+//transformar base 10 para binario: 12:00 23/08/2024
+//transformar base 10 para hexadecimal: 15:30 23/08/2024
+//transformar base 10 para octal: 15:45 23/08/2024
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -47,12 +54,34 @@
         *tamanho = i;
     }
 
+    void transforma_bcd(int n,char resultadoBCD[], int *tamanho) {
+
+        int i = 0;
+        int digits[10];
+        int num_digits = 0;
+
+        while (n > 0) {
+            digits[num_digits++] = n % 10;
+            n /= 10;
+        }
+
+        for (int j = num_digits - 1; j >= 0; j--) {
+            int digito = digits[j];
+
+            for (int k = 3; k >= 0; k--) {
+                resultadoBCD[i + k] = (digito % 2) + '0';
+                digito /= 2;
+            }
+            i += 4;
+        }
+
+        *tamanho = i;
+    }
+
 int main(void) {
 
-    int opcao,numero;
-    int resultado[32];
-    char resultadohexa[32];
-    int tamanho = 0;
+    int opcao,numero,resultado[32],tamanho=0;
+    char resultadohexa[32],resultadoBCD[40];
 
     do {
         printf("Escolha qual operacao deseja fazer: \n\nBase 10 para Binario: 1\n\nBase 10 para Octal: 2\n\nBase 10 para Hexadecimal: 3\n\nBase 10 para codigo BCD: 4\n\nBase 10 para base com sinal com 16 bits: 5\n\nreal em decimal para float e double: 6");
@@ -106,6 +135,19 @@ int main(void) {
         case 4:
             printf("Digite um numero na Base 10: ");
             scanf("%d",&numero);
+
+            transforma_bcd(numero,resultadoBCD, &tamanho);
+
+            printf("Resultado em BCD: ");
+
+            for (int i = 0; i < tamanho; i++) {
+                if (i > 0 && i % 4 == 0) {
+                    printf(" ");
+                }
+                printf("%c", resultadoBCD[i]);
+            }
+
+            break;
 
         case 5:
             printf("Digite um numero na Base 10: ");
